c9f5fa815a0352e8b2676e13ce7bc6b11f1c9777
charlie-a-gao <gaocharl17@grinnell.edu>
2014-09-02 11:46:40 -0500

    Added hello world

---
+++ src/edu/grinnell/csc207/gaocharl17/hello/HelloWorld.java
@@ -0,0 +1,9 @@
+package edu.grinnell.csc207.gaocharl17.hello;
+
+public class HelloWorld
+{
+  public static void main (String[] args) 
+  {
+    System.out.println ("Hello, GitHub!");
+  } // main(String[])
+}
